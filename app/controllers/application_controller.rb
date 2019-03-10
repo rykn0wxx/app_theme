5fc193cd6abd46025ead43889ca6f19a452e79f7
rykn0wxx <raurici666@gmail.com>
2019-03-09 23:59:36 -0500

    to be continue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :role, :password, :password_confirmation])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
       devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
     end
